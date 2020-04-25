cb0c380fbef8b976418b48c25dd3e46f029ffb25
NguyenMinhDuc <duc2820@gmail.com>
2020-04-26 00:45:13 +0700

    update ui

--- src/EditVideo/Inspector.tsx
+++ src/EditVideo/Inspector.tsx
@@ -35,7 +35,7 @@ const FormControl: any = styled.div`
     margin: ${(e : any) => e.notSpacing ?  '0px' : '10px 0px'};
 `;
 
-const Inspector = ({handleMark} : any) => {
+const Inspector = ({setQuestions , answers, changeAnswer , question, data} : any) => {
   return (
     <div
       style={{
@@ -45,54 +45,60 @@ const Inspector = ({handleMark} : any) => {
         boxShadow: `0px 2px 2px -1px rgba(0,0,0,0.1), 0px 2px 5px 0px rgba(0,0,0,0.1), 0px 1px 10px 0px rgba(0,0,0,0.1)`,
       }}
     >
-      <div style={{ padding: "16px 32px" }}>
-        <Typography
-          variant="subtitle1"
-          style={{
-            fontSize: "14px",
-            paddingBottom: "8px",
-            textAlign: "left",
-            fontWeight: 700,
-          }}
-        >
-          <FormControl>
-            <Label>Name Video</Label>
-            <Input />
-          </FormControl>
-          <FormControl>
-            <Label>Time Line</Label>
-            <Input />
-          </FormControl>
-          <FormControl>
-            <Label>Question?</Label>
-            <Input />
-          </FormControl>
-          <FormControl>
-            <Label>Result</Label>
-          {[1,2,3,4].map(item =>   <FormControl row notSpacing>
-              <Radio
-                checked={true}
-                onChange={(e) => console.log(e)}
-                value="b"
-                name="radio-button-demo"
-                inputProps={{ "aria-label": "B" }}
-              />
-              <FormControl style={{ display: "block" , width : '100%' }}>
-                <Input style={{ width: "100%" }} />
-                <DeleteIcon
-                  style={{
-                    position: "absolute",
-                    transform: "translate(-122% , 19%)",
-                  }}
-                />
-              </FormControl>
-            </FormControl>)}
-          </FormControl>
-        <div>
-        <Button onClick={handleMark}>Finish</Button>
+         <div style={{ padding: "16px 32px" }}>
+            <Typography
+                variant="subtitle1"
+                style={{
+                    fontSize: "14px",
+                    paddingBottom: "8px",
+                    textAlign: "left",
+                    fontWeight: 700,
+                }}
+            >
+
+                <FormControl>
+                    <Label>Name Video</Label>
+                    <Input value={data.name_video} />
+                </FormControl>
+                <FormControl>
+                    <Label>Time Line</Label>
+                    <Input  value={question && question.time_line  || "" } />
+                </FormControl>
+                <FormControl>
+                    <Label>Question?</Label>
+                    <Input  value={question && question.question || ""} onChange={() => {
+                    }} />
+                </FormControl>
+                <FormControl>
+                    <Label>Result</Label>
+                    {(question && question.answers ||  ["","","",""]).map((item : any, key : number ) =>   <FormControl row notSpacing>
+                        <Radio
+                            checked={true}
+                            onChange={(e) => console.log(e)}
+                            value="b"
+                            name="radio-button-demo"
+                            inputProps={{ "aria-label": "B" }}
+                        />
+                        <FormControl style={{ display: "block" , width : '100%' }}>
+                            <Input style={{ width: "100%" }} value={item} onChange ={(e: any) => {
+                                let newAnswers  =  [...answers]
+                                newAnswers[key] = e.target.value
+                                changeAnswer(newAnswers)
+                            }} />
+                            <DeleteIcon
+                                style={{
+                                    position: "absolute",
+                                    transform: "translate(-122% , 19%)",
+                                }}
+                            />
+                        </FormControl>
+                    </FormControl>)}
+                </FormControl>
+                <div>
+                    <Button onClick={setQuestions}>Finish</Button>
+                </div>
+            </Typography>
         </div>
-        </Typography>
-      </div>
       
     </div>
   );
--- src/EditVideo/index.tsx
+++ src/EditVideo/index.tsx
@@ -3,70 +3,104 @@ import { useVideo } from "react-use";
 import IconButton from "@material-ui/core/IconButton";
 import PlayArrow from "@material-ui/icons/PlayArrow";
 import Pause from "@material-ui/icons/Pause";
-import Inspector from './Inspector'
+import Inspector from "./Inspector";
 import PrettoSlider from "../components/UI/Slider";
 import styled from "styled-components";
-const data = {
-  id: "1",
-  video_url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4",
-  questions: [
-    {
-      id: "1",
-      time: 5,
+import uuid from "uuid";
+// const Mockdata = ;
+const Video  = ({src}:{src : string}) =>   <video
+    width="840"
+    src={src}
+    // controls
+    style={{
+        borderRadius: "4px",
+        boxShadow: `0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)`,
+    }}
+/>
+const EditView: React.FC<{}> = () => {
+    const [data, setData] = React.useState({
+        id: "1",
+        name_video : "Học tiếng",
+        time_line : "alo alo ",
+        video_url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4",
+        questions : [
+            {
+                id: "1",
+                time: 5,
+                type: "quiz",
+                question: "Chọn đáp án thể hiện đúng cách dùng mạo từ a, an?",
+                answers: ["A: a pen", "B: a hour", "C: an car", "D: a eraser"],
+                correct: 0,
+                note:
+                    'Giải thích: a, an đứng trước danh từ số ít, đếm được, an đứng trước nguyên âm phiên âm "ueoai".',
+            }
+        ]
+
+    }) as any
+    const [video, state, controls, ref] = useVideo(<Video src={data.video_url} />) as any;
+  const [timePer, setTimePer] = useState(0);
+
+    const [answers , setAnswer] = React.useState(["A: a pen", "B: a hour", "C: an car", "D: a eraser"])
+    const [questions , setQuestions ] = React.useState( [
+        {
+            id: "1",
+            time: 5,
+            type: "quiz",
+            time_line : "time line",
+
+            question: "Chọn đáp án thể hiện đúng cách dùng mạo từ a, an?",
+            answers: ["A: a pen", "B: a hour", "C: an car", "D: a eraser"],
+            correct: 0,
+            note:
+                'Giải thích: a, an đứng trước danh từ số ít, đếm được, an đứng trước nguyên âm phiên âm "ueoai".',
+        }
+    ]) as any
+    const [question , setQuestion ] = React.useState(null) as any
+
+  const handleChange = (event: any, value: any) => {
+    const timeNext = (value / 100) * state.duration;
+    setTimePer(value);
+    controls.seek(timeNext);
+    console.log(
+      "timeNext",
+      timeNext,
+      value,
+      state.duration,
+      state.time,
+      timeNext === state.time
+    );
+  };
+
+  const handleQuestions = () => {
+    let newData = [...questions];
+    const newQuestion = {
+      id: newData.length + 1 + '',
+      time: timePer,
       type: "quiz",
       question: "Chọn đáp án thể hiện đúng cách dùng mạo từ a, an?",
-      answers: ["A: a pen", "B: a hour", "C: an car", "D: a eraser"],
+      answers: answers,
       correct: 0,
       note:
         'Giải thích: a, an đứng trước danh từ số ít, đếm được, an đứng trước nguyên âm phiên âm "ueoai".',
-    },
-  ],
-};
-
-const StudentView: React.FC<{}> = () => {
-  const [timePer, setTimePer] = useState(0);
-  const [marks , setMarks] = React.useState([]) as any
-  const [video, state, controls , ref]  = useVideo(
-    <video
-      width="840"
-      src={data.video_url}
-      // controls
-      style={{
-        borderRadius: "4px",
-        boxShadow: `0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)`,
-      }}
-    />
-  ) as any
-  React.useEffect(() => {
-    console.log("controls",controls , state)
-  },[])
-  const handleChange = (event: any, value: any) => {
-    const timeNext = (value / 100) * state.duration
-    setTimePer(value)
-    controls.seek(timeNext)
-    console.log("timeNext",timeNext , value , state.duration , state.time ,  timeNext === state.time)
-};
-const  handleMark =  () => {
-    setMarks([...marks , ...[{value : timePer  , label : state.time + '' }]])
-}
-
-//   useEffect(() => {
-//     // eslint-disable-next-line array-callback-return
-// data.questions.map((item) => {
-//       if (item.time === Math.floor(state.time)) {
-//         controls.pause();
-//         setShowQuestion(true);
-//       } else {
-//         // controls.play();
-//         setShowQuestion(false);
-//       }
-//     });
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [Math.floor(state.time)]);
-  // const [marks , setMarks] = React.useState([]) as any
-  // const handleMark = () => {
-  //   // setMarks([...marks , ...[{value : perVideo  , label : state.time + '' }]])
-  // };
+    };
+    newData = [...newData, ...[newQuestion]];
+    setQuestions(newData);
+  };
+    useEffect(() => {
+        console.log("casnkjcnaskjc",data)
+        // eslint-disable-next-line array-callback-return
+       if(questions && questions.length > 0) {
+           questions.map((item : any) => {
+               if (item.time === Math.floor(timePer)) {
+                   setQuestion(item)
+               }
+               else {
+                   setQuestion(null)
+               }
+           })
+       }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [Math.floor(timePer)])
   return (
     <div style={{ paddingTop: "56px" }}>
       <div
@@ -93,10 +127,7 @@ const  handleMark =  () => {
               {/* video */}
               <div>{video}</div>
 
-              {/* render question */}
-              <div>
-            
-              </div>
+              <div></div>
             </div>
           </div>
 
@@ -137,18 +168,17 @@ const  handleMark =  () => {
                 valueLabelDisplay="auto"
                 aria-label="pretto slider"
                 value={timePer}
-                marks={marks}
-                style={{ marginRight: "32px"  }}
+                marks={ questions.map((item:any) => ({ value: item.time , label : item.item }))}
+                style={{ marginRight: "32px" }}
               />
             </div>
           </div>
         </div>
 
-        <Inspector handleMark={handleMark} />
-      
+          <Inspector data={data} setQuestions={handleQuestions} answers={answers} changeAnswer = {setAnswer} question={question} />
       </div>
     </div>
   );
 };
 
-export default StudentView;
+export default EditView;
