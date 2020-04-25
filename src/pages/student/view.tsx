3db9f39dcf865b4b7647b6bb54723b4cdc740d16
Truong An <ancs21.ps@gmail.com>
2020-04-25 16:59:08 +0700

    update check result

--- src/pages/student/view.tsx
+++ src/pages/student/view.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useVideo } from 'react-use'
-import { Slider, withStyles, Typography, Avatar } from '@material-ui/core'
+import {
+  Slider,
+  withStyles,
+  Typography,
+  Avatar,
+  Button,
+} from '@material-ui/core'
 import IconButton from '@material-ui/core/IconButton'
 import PlayArrow from '@material-ui/icons/PlayArrow'
 import Pause from '@material-ui/icons/Pause'
@@ -8,6 +14,11 @@ import Pause from '@material-ui/icons/Pause'
 import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
 
 const data = {
   id: '1',
@@ -15,9 +26,20 @@ const data = {
   questions: [
     {
       id: '1',
+      tag: 'grammar',
       time: 5,
       type: 'quiz',
       question: 'Chọn đáp án thể hiện đúng cách dùng mạo từ a, an?',
+      answers: ['A: a pen', 'B: a hour', 'C: an car', 'D: a eraser'],
+      correct: 0,
+      note:
+        'Giải thích: a, an đứng trước danh từ số ít, đếm được, an đứng trước nguyên âm phiên âm "ueoai".',
+    },
+    {
+      id: '2',
+      time: 15,
+      type: 'quiz',
+      question: 'Chsadsadsad?',
       answers: ['A: a pen', 'B: a hour', 'C: an car', 'D: a eraser'],
       correct: 0,
       note:
@@ -71,6 +93,8 @@ const PrettoSlider = withStyles({
 
 const StudentView: React.FC<{}> = () => {
   const [showQuestion, setShowQuestion] = useState<Boolean>(false)
+  const [showCorrect, setShowCorrect] = useState(false)
+  const [showIncorrect, setshowIncorrect] = useState(false)
   const [video, state, controls] = useVideo(
     <video
       width="840"
@@ -83,9 +107,13 @@ const StudentView: React.FC<{}> = () => {
     />
   )
 
-  const handleRadioChange = (e: any) => {
+  const handleRadioChange = (e: any, item: any) => {
     e.preventDefault()
-    console.log(e.target.value)
+    if (parseInt(e.target.value) === item.correct) {
+      setShowCorrect(true)
+    } else {
+      setshowIncorrect(true)
+    }
   }
   useEffect(() => {
     // eslint-disable-next-line array-callback-return
@@ -93,9 +121,6 @@ const StudentView: React.FC<{}> = () => {
       if (item.time === Math.floor(state.time)) {
         controls.pause()
         setShowQuestion(true)
-      } else {
-        controls.play()
-        setShowQuestion(false)
       }
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -140,84 +165,87 @@ const StudentView: React.FC<{}> = () => {
                       opacity: '.8',
                       color: 'white',
                       padding: '16px 32px',
+                      textAlign: 'center',
                     }}
                   >
-                    {data.questions.map((item) => (
-                      <div key={item.id}>
-                        {item.time === Math.floor(state.time) && (
-                          <>
-                            <Typography variant="h5">
-                              {item.question}
-                            </Typography>
-                            <div>
-                              <RadioGroup
-                                aria-label="quiz"
-                                name="quiz"
-                                // value={value}
-                                onChange={handleRadioChange}
-                                style={{
-                                  display: 'flex',
-                                  justifyContent: 'space-between',
-                                }}
-                              >
-                                <div>
-                                  <FormControlLabel
-                                    value="0"
-                                    control={<Radio />}
-                                    label={item.answers[0]}
-                                    style={{
-                                      backgroundColor: '#e15a00',
-                                      color: 'white',
-                                      width: '200px',
-                                      borderRadius: '8px',
-                                      margin: '10px',
-                                    }}
-                                  />
-                                  <FormControlLabel
-                                    value="1"
-                                    control={<Radio />}
-                                    label={item.answers[1]}
-                                    style={{
-                                      backgroundColor: '#e15a00',
-                                      color: 'white',
-                                      width: '200px',
-                                      borderRadius: '8px',
-                                      margin: '10px',
-                                    }}
-                                  />
-                                </div>
-                                <div>
-                                  <FormControlLabel
-                                    value="2"
-                                    control={<Radio />}
-                                    label={item.answers[2]}
-                                    style={{
-                                      backgroundColor: '#e15a00',
-                                      color: 'white',
-                                      width: '200px',
-                                      borderRadius: '8px',
-                                      margin: '10px',
-                                    }}
-                                  />
-                                  <FormControlLabel
-                                    value="3"
-                                    control={<Radio />}
-                                    label={item.answers[3]}
-                                    style={{
-                                      backgroundColor: '#e15a00',
-                                      color: 'white',
-                                      width: '200px',
-                                      borderRadius: '8px',
-                                      margin: '10px',
-                                    }}
-                                  />
-                                </div>
-                              </RadioGroup>
-                            </div>
-                          </>
-                        )}
-                      </div>
-                    ))}
+                    {data.questions.map((item) => {
+                      return (
+                        <div key={item.id}>
+                          {item.time === Math.floor(state.time) && (
+                            <>
+                              <Typography variant="h5">
+                                {item.question}
+                              </Typography>
+                              <div>
+                                <RadioGroup
+                                  aria-label="quiz"
+                                  name="quiz"
+                                  // value={value}
+                                  onChange={(e) => handleRadioChange(e, item)}
+                                  style={{
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                  }}
+                                >
+                                  <div>
+                                    <FormControlLabel
+                                      value="0"
+                                      control={<Radio />}
+                                      label={item.answers[0]}
+                                      style={{
+                                        backgroundColor: '#e15a00',
+                                        color: 'white',
+                                        width: '200px',
+                                        borderRadius: '8px',
+                                        margin: '10px',
+                                      }}
+                                    />
+                                    <FormControlLabel
+                                      value="1"
+                                      control={<Radio />}
+                                      label={item.answers[1]}
+                                      style={{
+                                        backgroundColor: '#e15a00',
+                                        color: 'white',
+                                        width: '200px',
+                                        borderRadius: '8px',
+                                        margin: '10px',
+                                      }}
+                                    />
+                                  </div>
+                                  <div>
+                                    <FormControlLabel
+                                      value="2"
+                                      control={<Radio />}
+                                      label={item.answers[2]}
+                                      style={{
+                                        backgroundColor: '#e15a00',
+                                        color: 'white',
+                                        width: '200px',
+                                        borderRadius: '8px',
+                                        margin: '10px',
+                                      }}
+                                    />
+                                    <FormControlLabel
+                                      value="3"
+                                      control={<Radio />}
+                                      label={item.answers[3]}
+                                      style={{
+                                        backgroundColor: '#e15a00',
+                                        color: 'white',
+                                        width: '200px',
+                                        borderRadius: '8px',
+                                        margin: '10px',
+                                      }}
+                                    />
+                                  </div>
+                                </RadioGroup>
+                              </div>
+                            </>
+                          )}
+                        </div>
+                      )
+                    })}
                   </div>
                 )}
               </div>
@@ -240,7 +268,10 @@ const StudentView: React.FC<{}> = () => {
                   color="primary"
                   aria-label="play"
                   style={{ marginRight: '16px' }}
-                  onClick={controls.play}
+                  onClick={() => {
+                    controls.play()
+                    setShowQuestion(false)
+                  }}
                 >
                   <PlayArrow fontSize="large" />
                 </IconButton>
@@ -331,6 +362,83 @@ const StudentView: React.FC<{}> = () => {
           {/* modal tracking */}
         </div>
       </div>
+      {/* modal answer */}
+      {/* true */}
+      <Dialog
+        open={showCorrect}
+        onClose={() => {
+          setShowCorrect(false)
+          controls.play()
+          setShowQuestion(false)
+        }}
+        disableAutoFocus
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Correct</DialogTitle>
+        <DialogContent>
+          <DialogContentText
+            id="alert-dialog-description"
+            style={{ width: '300px', textAlign: 'center' }}
+          >
+            <img
+              width="200"
+              src="https://cdn3.iconfinder.com/data/icons/flat-actions-icons-9/792/Tick_Mark_Dark-512.png"
+              alt=""
+            />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            color="primary"
+            onClick={() => {
+              setShowCorrect(false)
+              controls.play()
+              setShowQuestion(false)
+            }}
+          >
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* false */}
+      <Dialog
+        open={showIncorrect}
+        onClose={() => {
+          setshowIncorrect(false)
+          controls.play()
+          setShowQuestion(false)
+        }}
+        disableAutoFocus
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Incorrect</DialogTitle>
+        <DialogContent>
+          <DialogContentText
+            id="alert-dialog-description"
+            style={{ width: '300px', textAlign: 'center' }}
+          >
+            <img
+              width="200"
+              src="https://cdn3.iconfinder.com/data/icons/flat-actions-icons-9/792/Close_Icon_Dark-512.png"
+              alt=""
+            />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            color="primary"
+            onClick={() => {
+              setshowIncorrect(false)
+              controls.play()
+              setShowQuestion(false)
+            }}
+          >
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
